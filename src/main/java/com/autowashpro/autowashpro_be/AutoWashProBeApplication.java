98e43937148bd8ff896601d1df33fd9b32be7c35
Duong Tuan Khoan <duongtuankhoan123@gmail.com>
2026-06-12 18:42:02 +0700

    Initial commit: AutoWashPro BE project

---
+++ src/main/java/com/autowashpro/autowashpro_be/AutoWashProBeApplication.java
@@ -0,0 +1,13 @@
+package com.autowashpro.autowashpro_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoWashProBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoWashProBeApplication.class, args);
+    }
+
+}
